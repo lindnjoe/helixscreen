e5d532f7caebd6431e626474924db2f6d8adaa69
Preston Brown <pbrown@brown-house.net>
2025-12-28 14:01:03 -0500

    refactor: remove WIN32 ifdef, use localtime_r directly

--- src/print_start_enhancer.cpp
+++ src/print_start_enhancer.cpp
@@ -292,11 +292,7 @@ std::string PrintStartEnhancer::generate_backup_filename(const std::string& sour
     auto now = std::chrono::system_clock::now();
     auto time_t_now = std::chrono::system_clock::to_time_t(now);
     std::tm tm_now{};
-#ifdef _WIN32
-    localtime_s(&tm_now, &time_t_now);
-#else
     localtime_r(&time_t_now, &tm_now);
-#endif
 
     std::ostringstream ss;
     ss << source_file << ".backup." << std::put_time(&tm_now, "%Y%m%d_%H%M%S");
